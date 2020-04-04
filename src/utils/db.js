3723d7a96cf82d15b90fadfbf607fcd693d23caf
Tesfaye Belachew Abebe <get.tesfaye.belachew@gmail.com>
2020-04-04 12:53:26 +0300

    added mongodb uri from mlab

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,20 +1,21 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
+const DB_URI_PROD = `mongodb://tesfanur:tesfanur#234@ds259577.mlab.com:59577/ltmath`;
 
-const DB_CONNECTION = process.env.DB_CONNECTION;
+const DB_CONNECTION = process.env.DB_CONNECTION || DB_URI_PROD;
 function connectToDB() {
   mongoose.Promise = global.Promise;
   mongoose
     .connect(DB_CONNECTION, {
       useCreateIndex: true,
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
     })
     .then(() => console.log("✔️  Now connected to MongoDB!"))
-    .catch(err => console.error("Something went wrong", err));
+    .catch((err) => console.error("Something went wrong", err));
 
-  mongoose.connection.on("error", err => {
+  mongoose.connection.on("error", (err) => {
     console.log(`DB connection error: ${err.message}`);
   });
 }
